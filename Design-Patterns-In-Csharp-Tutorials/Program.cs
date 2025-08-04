102254514edff4c0cb785b57e107cce4bd1fc4e7
Gilvan Almeida <GilvanAlmeida@screendragon.com>
2025-08-04 22:27:46 +0100

    Encapsulation

---
+++ Design-Patterns-In-Csharp-Tutorials/Program.cs
@@ -0,0 +1,14 @@
+﻿// Encapsulation
+
+using Design_Patterns_In_Csharp_Tutorials.OopPrinciples.Encapsulation;
+
+BankAccount BankAccount = new BankAccount(100);
+System.Console.WriteLine(BankAccount.GetBalance());
+BankAccount.Deposit(50);
+System.Console.WriteLine(BankAccount.GetBalance());
+BankAccount.Withdraw(200);
+System.Console.WriteLine(BankAccount.GetBalance());
+
+// BadBankAccount badBankAccount = new BadBankAccount();
+// badBankAccount.balance = 100;
+// System.Console.WriteLine(badBankAccount.balance);
---
+++ Design-Patterns-In-Csharp-Tutorials/src/OopPrinciples/Encapsulation/BadBankAccount.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Design_Patterns_In_Csharp_Tutorials.OopPrinciples.Encapsulation
+{
+    public class BadBankAccount
+    {
+        public decimal balance;
+        public BadBankAccount()
+        {
+            // Construtor
+        }
+    }
+}
